9fcdc1906d1912feb0b8acba24ceffaac7dea986
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2020-05-04 05:14:30 +0900

    Done first page

---
+++ cplusplus.com/program_structure/initial.cpp
@@ -0,0 +1,12 @@
+// http://www.cplusplus.com/doc/tutorial/program_structure/
+# include <iostream>
+/*
+  multipul lines of comments 
+  it is same as JavaScript
+*/
+
+int main()
+{
+  std::cout << "Hello, C++! ";
+  std::cout << "I'm a C++ Coder";
+}
---
+++ cplusplus.com/program_structure/using_namespace.cpp
@@ -0,0 +1,9 @@
+// second program
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello C++! ";
+  cout << "I'm a C++ Program";
+}
