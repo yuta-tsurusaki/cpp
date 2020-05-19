ba49bc2efd05760c5204ee901d0ca9cd8c2ca329
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2020-05-20 08:49:43 +0900

    Finish namespace

---
+++ cplusplus.com/namespace/static_vs_automatic.cpp
@@ -0,0 +1,14 @@
+// static vs automatic storage
+#include <iostream>
+using namespace std;
+
+int x;
+
+int main ()
+{
+  int y, z;
+  cout << x << '\n';
+  cout << y << '\n';
+  cout << z << '\n';
+  return 0;
+}
